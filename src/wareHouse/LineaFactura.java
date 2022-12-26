1e61cb0d411a317f9e29deb386fe3d303ff878af
nagor <nagor@DESKTOP-06>
2022-12-26 16:41:01 +0100

    getters and setters

--- src/wareHouse/LineaFactura.java
+++ src/wareHouse/LineaFactura.java
@@ -1,5 +1,33 @@
 package wareHouse;
 
-public class LineaFactura {
+import java.util.ArrayList;
 
+public class LineaFactura {
+ 
+	private int numero;
+	private ArrayList Articulo = new ArrayList();
+	private int cantidad;
+
+	
+	//getters and setters
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public ArrayList getArticulo() {
+		return Articulo;
+	}
+	public void setArticulo(ArrayList articulo) {
+		Articulo = articulo;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
 }
