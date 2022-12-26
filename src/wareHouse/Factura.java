e9138471bf72e38f6e6abb64637562bb1e9cbe1a
nagor <nagor@DESKTOP-06>
2022-12-26 16:06:36 +0100

    setters creado

--- src/wareHouse/Factura.java
+++ src/wareHouse/Factura.java
@@ -1,5 +1,48 @@
 package wareHouse;
 
+import java.util.Date;
+
 public class Factura {
 
+	private int IVA =21;
+	private int numero;
+	private  String nombreEmpresa;
+	private Date fecha =new Date();
+	private String concepto;
+
+	
+	//getter and setters
+	
+	public int getIVA() {
+		return IVA;
+	}
+	public void setIVA(int iVA) {
+		IVA = iVA;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getNombreEmpresa() {
+		return nombreEmpresa;
+	}
+	public void setNombreEmpresa(String nombreEmpresa) {
+		this.nombreEmpresa = nombreEmpresa;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getConcepto() {
+		return concepto;
+	}
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+	
+	
 }
