d1a903b21ba33994a455dab7d240cb724360ab8f
nagor <nagor@DESKTOP-06>
2022-12-30 11:22:49 +0100

    Añadir linea

--- src/wareHouse/Factura.java
+++ src/wareHouse/Factura.java
@@ -1,6 +1,11 @@
 package wareHouse;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
 
 public class Factura {
 
@@ -9,7 +14,7 @@ public class Factura {
 	private  String nombreEmpresa;
 	private Date fecha =new Date();
 	private String concepto;
-
+	private ArrayList <LineaFactura> lineas = new ArrayList<>();
 	
 	//getter and setters
 	
@@ -44,5 +49,75 @@ public class Factura {
 		this.concepto = concepto;
 	}
 	
+	//añadir una linea a la factura
+	public void addLinea(LineaFactura linea, Articulo articulo ){
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("DATOS DE LINEAFACTURA");
+		System.out.println("Indica el numero de la linea");
+		int numeroFactLin= Integer.parseInt(teclado.nextLine());
+		//parte del articulo
+		System.out.println("Indica el numero del articulo");
+		String articuloNum=teclado.nextLine();
+		System.out.println("Indica el nombre del producto");
+		String articuloNom=teclado.nextLine();
+		System.out.println("Introduce la cantidad");
+		System.out.println("Introduce la marca del producto");
+		String articuloMarc=teclado.nextLine();
+		System.out.println("Indica la capidad que tiene la botella");
+		int articuloCap=Integer.parseInt(teclado.nextLine());
+		System.out.println("Indica el precio del articulo");
+		double articuloPrec=Double.parseDouble(teclado.nextLine());
+		//
+		System.out.println("Introduce la cantidad");
+		int cantidadFacLin= Integer.parseInt(teclado.nextLine());
+		linea.setNumero(numeroFactLin);
+		linea.setArticulo(articuloNum,articuloNom,articuloMarc, articuloCap,articuloPrec);
+	
+		linea.setCantidad(cantidadFacLin);
+		lineas.add(linea);
+	}
+		/*
+	public double precioTotal() {
+		double total=0;
+		for (int i = 0; i < lineas.size(); i++) {
+			total=total+lineas.get(i).precioTotal();
+		}
+		
+		return total;
+	}
+*/
+	//hacer un iterator para borrar linea
+
+	public void elimLinea (int  linea) {
+		Iterator<LineaFactura> iter = lineas.iterator();
+		while(iter.hasNext()) {
+			int numLinea=iter.next().getNumero();
+			if(numLinea==linea) {
+				iter.remove();
+			}
+		}
+	}
+
+	
+	
+	/*
+	public void imprimir() {
+		System.out.println("DATOS DE LA FACTURA");
+		System.out.println("El numero de la linea es:  "+this.numero);
+		System.out.println("El nombre de la empresa es: "+this.nombreEmpresa);
+		System.out.println("La fecha es: "+this.fecha);
+		System.out.println("El concepto es: "+this.concepto);
+		System.out.println("-----------------------------------");
+		
+		for (LineaFactura lineaFactura : lineas) {
+			System.out.println("DATOS DE LA LINEA");
+			System.out.println(lineaFactura);
+		}
+	}
+	
+	*/
+
+
+
 	
 }
