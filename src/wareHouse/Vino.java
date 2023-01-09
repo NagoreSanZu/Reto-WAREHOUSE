b0401717d22c4c44c394d6d20486a0c308770927
Nagore Sanchez <Nagore Sanchez@HZ114832.H012108.NET>
2023-01-09 09:32:09 +0100

    es saludable

--- src/wareHouse/Vino.java
+++ src/wareHouse/Vino.java
@@ -1,76 +1,92 @@
 package wareHouse;
 
-public class Vino extends Articulo  implements Alcoholico{
+public class Vino extends Articulo implements Alcoholico {
 
 	private String color;
 	private String origen;
 	private int anio;
 	private String tipoDeUva;
 	private double gradosAlcohol;
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
+
 	public String getOrigen() {
 		return origen;
 	}
+
 	public void setOrigen(String origen) {
 		this.origen = origen;
 	}
+
 	public int getAnio() {
 		return anio;
 	}
+
 	public void setAnio(int anio) {
 		this.anio = anio;
 	}
+
 	public String getTipoDeUva() {
 		return tipoDeUva;
 	}
+
 	public void setTipoDeUva(String tipoDeUva) {
 		this.tipoDeUva = tipoDeUva;
 	}
+
 	public double getGradosAlcohol() {
 		return gradosAlcohol;
 	}
+
 	public void setGradosAlcohol(double gradosAlcohol) {
 		this.gradosAlcohol = gradosAlcohol;
 	}
+
 	@Override
 	public String toString() {
 		return "Vino color=" + color + ", origen=" + origen + ", anio=" + anio + ", tipoDeUva=" + tipoDeUva
-				+ ", gradosAlcohol=" + gradosAlcohol ;
+				+ ", gradosAlcohol=" + gradosAlcohol;
 	}
+
 	@Override
 	public boolean esFuerte() {
 		// TODO Auto-generated method stub
-		if (this.gradosAlcohol > 13.5 ) {
+		if (this.gradosAlcohol > 13.5)
 			return true;
 
-		}else {
-			return false;
+		return false;
 
-		}
-		
 	}
+
 	@Override
 	public double calcularTasa() {
-		
-		double tasaPorL, totalV=0;
-		if (this.gradosAlcohol > 13.5 ) {
-		tasaPorL=	TASA_BEBIDAS_FUERTES/100;
-		totalV= getCapacidadBotella()*tasaPorL;
 
-		}else if (this.gradosAlcohol < 13.5 ){
-			tasaPorL=	TASA_BEBIDAS_SUAVES/100;
-			totalV= getCapacidadBotella()*tasaPorL;
+		double tasaPorL, totalV = 0;
+		if (this.gradosAlcohol > 13.5) {
+			tasaPorL = TASA_BEBIDAS_FUERTES / 100;
+			totalV = getCapacidadBotella() * tasaPorL;
+
+		} else if (this.gradosAlcohol < 13.5) {
+			tasaPorL = TASA_BEBIDAS_SUAVES / 100;
+			totalV = getCapacidadBotella() * tasaPorL;
 
 		}
 		return totalV;
 	}
-	
-   
-	
-	
+
+
+	@Override
+	public boolean saludable() {
+		boolean esSaludableV =false;
+		if (this.origen.equals("Navarra")) 
+			esSaludableV =true;
+			
+		return esSaludableV;
+	}
 }
