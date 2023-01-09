43e51f596c569a9bfbd22f953974a1202e34ea84
nagor <nagor@DESKTOP-06>
2023-01-09 18:06:34 +0100

    constructor con parametros

--- src/wareHouse/Vino.java
+++ src/wareHouse/Vino.java
@@ -1,5 +1,7 @@
 package wareHouse;
 
+import java.util.Scanner;
+
 public class Vino extends Articulo implements Alcoholico {
 
 	private String color;
@@ -7,6 +9,17 @@ public class Vino extends Articulo implements Alcoholico {
 	private int anio;
 	private String tipoDeUva;
 	private double gradosAlcohol;
+	
+	public Vino(String name, String code, String mark, int capacidadBotella, double preciom, int stock,
+			String color, String origen,int anio,String tipoDeUva, double gradosAlcohol ) {
+		super(name, code, mark, capacidadBotella, preciom, stock);
+		this.color=color;
+		this.origen=origen;
+		this.anio=anio;
+		this.tipoDeUva=tipoDeUva;
+		this.gradosAlcohol=gradosAlcohol;
+		
+	}
 
 	public String getColor() {
 		return color;
@@ -79,7 +92,25 @@ public class Vino extends Articulo implements Alcoholico {
 		}
 		return totalV;
 	}
-
+	public void agregarVino() {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("INCREMENTAR VINO");
+		System.out.println("De que color es el vino?");
+		String colorVino=teclado.nextLine();
+		System.out.println("De que origen es el vino?");
+		String origenVino=teclado.nextLine();
+		System.out.println("De que ali es el vino?");
+		int anoVino =Integer.parseInt(teclado.nextLine());
+		System.out.println("Que tipo de uva tiene?");
+		String tipoUva =teclado.nextLine();
+		System.out.println("Cuantos grados tine el alchol?");
+		double gradosVino =Double.parseDouble(teclado.nextLine());
+		this.color=colorVino;
+		this.origen=origenVino;
+		this.anio=anoVino;
+		this.tipoDeUva=tipoUva;
+		this.gradosAlcohol=gradosVino;
+	}
 
 	@Override
 	public boolean saludable() {
