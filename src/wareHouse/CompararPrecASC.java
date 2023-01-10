5c9a9cf274ca2841eaaf161b56e726633f7c2e29
Nagore Sanchez <Nagore Sanchez@HZ114832.H012108.NET>
2023-01-10 09:35:15 +0100

    metodo de comparar asc

---
+++ src/wareHouse/CompararPrecASC.java
@@ -0,0 +1,15 @@
+package wareHouse;
+
+import java.util.Comparator;
+
+public class CompararPrecASC implements Comparator<Articulo> {
+
+	@Override
+	public int compare(Articulo o1, Articulo o2) {
+		if(o1.getPrecio() > o2.getPrecio()) return 1;
+		
+		return -1;			
+	}
+
+
+}
