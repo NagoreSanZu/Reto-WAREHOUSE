c2015d17f1e9e002a1ea8e12f364138f59ce5e09
Nagore Sanchez <Nagore Sanchez@HZ114832.H012108.NET>
2023-01-12 10:54:27 +0100

    dentro de realizar venta menu creado

--- src/wareHouse/GestorAlmacenApp.java
+++ src/wareHouse/GestorAlmacenApp.java
@@ -1,10 +1,11 @@
 package wareHouse;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class GestorAlmacenApp {
 
-	public void run() {
+	public void run() throws ParseException {
 		
 		final int OPCION_UNO = 1;
 		final int OPCION_DOS = 2;
@@ -29,8 +30,56 @@ public class GestorAlmacenApp {
 
 			switch (opcion_menu) {
 			case OPCION_UNO:
-				System.out.println("primera opcion seleccionada\n");
+				Factura pedirFactura = new Factura();
+				pedirFactura.pedirDatos();
+				
+				final int OPCION_BAT = 1;
+				final int OPCION_BI = 2;
+				final int OPCION_HIRU = 3;
+				final int OPCION_LAU = 4;
+				final int AGUR = 0;
+
+				
+				int opcion_factura;
+
+				do {
+					System.out.println("------MENU-------");
+					System.out.println(OPCION_BAT + ". Añadir una line a la factura");
+					System.out.println(OPCION_BI + ". Eliminar linea");
+					System.out.println(OPCION_HIRU + ". Guardar linea");
+					System.out.println(OPCION_LAU + ". Mostrar factura");
+					System.out.println(AGUR + ". Salir");
+					System.out.println("Elije una de las opciones");
+					opcion_factura = Integer.parseInt(scan.nextLine());
+
+					switch (opcion_factura) {
+					case OPCION_BAT:
+						System.out.println("primera opcion seleccionada\n");
+						break;
+					case OPCION_BI:
+						System.out.println("segunda opcion seleccionada\n");
+						break;
+					case OPCION_HIRU:
+						System.out.println("tercera opcion seleccionada\n");
+						break;
+					case OPCION_LAU:
+						System.out.println("tercera opcion seleccionada\n");
+						break;
+					case AGUR:
+						System.out.println("ADIOS");
+						break;
+					default:
+						System.out.println("Opcion incorrecta!");
+					}
+
+				} while (opcion_menu != SALIR);
+				
 				break;
+				
+				
+				
+				
+				
 			case OPCION_DOS:
 				System.out.println("segunda opcion seleccionada\n");
 				break;
